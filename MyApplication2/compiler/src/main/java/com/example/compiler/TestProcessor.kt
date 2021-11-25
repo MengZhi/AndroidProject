56223bbd74ac3d717d6d03f931f3da3d2cc2b5ef
Dreams <zhimeng1991@gmail.com>
2021-11-26 00:11:34 +0800

    fix java poet dependence problem

--- MyApplication2/compiler/src/main/java/com/example/compiler/TestProcessor.kt
+++ MyApplication2/compiler/src/main/java/com/example/compiler/TestProcessor.kt
@@ -1,13 +1,22 @@
 package com.example.compiler
 
+import com.example.myarouter_annotations.MyARouter
+import com.squareup.javapoet.JavaFile
+import com.squareup.javapoet.MethodSpec
+import com.squareup.javapoet.TypeSpec
+import java.io.IOException
+import java.lang.reflect.Type
 import java.sql.Types
 import javax.annotation.processing.*
+import javax.lang.model.SourceVersion
+import javax.lang.model.element.Modifier
 import javax.lang.model.element.TypeElement
 import javax.lang.model.util.Elements
 import javax.tools.Diagnostic
 
 @SupportedAnnotationTypes("com.example.myarouter_annotations.MyARouter")
 @SupportedOptions("student")
+//@SupportedSourceVersion(SourceVersion.RELEASE_8)
 class TestProcessor : AbstractProcessor() {
     private lateinit var elementTool: Elements
 
@@ -29,13 +38,35 @@ class TestProcessor : AbstractProcessor() {
     }
 
     override fun process(
-        annotations: MutableSet<out TypeElement>?,
-        roundEnv: RoundEnvironment?
+        annotations: MutableSet<out TypeElement>,
+        roundEnv: RoundEnvironment
     ): Boolean {
 
         messager.printMessage(Diagnostic.Kind.NOTE, "===========")
 
-        return true
+        val elements = roundEnv.getElementsAnnotatedWith(MyARouter::class.java)
+        for (ele in elements) {
+            val mainMethod = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .addParameter(Array<System>::class.java, "args")
+                .addStatement("\$T.out.println(\$S)", System::class.java, "Hello Java Pots")
+                .build()
+
+            val testClass = TypeSpec.classBuilder("DreamsTest")
+                .addMethod(mainMethod)
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .build()
+
+            val packagef = JavaFile.builder("com.example.mengzhi.annotation", testClass).build()
+
+            try {
+                packagef.writeTo(filer)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                messager.printMessage(Diagnostic.Kind.NOTE, "generate file fail" + e.message)
+            }
+        }
+        return false
     }
 
     override fun getSupportedAnnotationTypes(): MutableSet<String> {
