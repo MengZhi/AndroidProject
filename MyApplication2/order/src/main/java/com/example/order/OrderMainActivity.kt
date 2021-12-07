7d5f0824f5c624ab65541a3241375bf1cdf58c28
Dreams <zhimeng1991@gmail.com>
2021-12-07 23:46:33 +0800

    add commonn ARouterApi order personal modules

---
+++ MyApplication2/order/src/main/java/com/example/order/OrderMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_main)
+    }
+}
---
+++ MyApplication2/order/src/main/java/com/example/order/debug/OrderDebugActivity.kt
@@ -0,0 +1,12 @@
+package com.example.order.debug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.order.R
+
+class OrderDebugActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_debug)
+    }
+}
